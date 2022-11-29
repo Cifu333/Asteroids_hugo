de85b498622aa3a7cf056cb390db0f6c04f95a41
ordenador clase <hugo.cifuentes@enti.cat>
2022-11-29 10:31:06 +0100

    tarea asterios movimiento y rotación,

---
+++ Assets/script/Teleport.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public float limit_x;
+    public float limit_y;
+    void Update()
+    {
+        if (transform.position.y > limit_y)
+        {
+        transform.position =new Vector3 (transform.position.x, -limit_y, transform.position.z);
+        }
+
+        if (transform.position.y < -limit_y)
+        {
+            transform.position = new Vector3(transform.position.x, limit_y, transform.position.z);
+        }
+
+    }
+}
